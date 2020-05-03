0ba9fb348127bd0b732681083a7e81e310738938
엄홍인 <zizon1wpxm@gmail.com>
2020-05-03 16:41:58 +0900

    list, search init

--- public/client/src/components/App.jsx
+++ public/client/src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import Search from "./search/Search";
+import List from "./list/List";
 
 function App() {
-  return <div className="App">lol</div>;
+  return (
+    <div className="App">
+      <Search />
+      <List />
+    </div>
+  );
 }
 
 export default App;
